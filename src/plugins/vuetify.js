331573d8faa90dcaa4cb068690efd89b68e1ad3f
wouerner <wouerner@protonmail.com>
2018-11-06 09:37:34 -0200

    inicio dos trabalhos

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import pt from 'vuetify/es5/locale/pt'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  }
+})
